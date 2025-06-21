47c97ef969db69ca00477597b4c04f05df48ad56
Kyle Godbey <me@kyle.ee>
2025-06-21 02:07:03 -0400

    final update for the night

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       define: {
-        // 'process.env.AUTH0_DOMAIN': JSON.stringify(env.AUTH0_DOMAIN),
-        // 'process.env.AUTH0_CLIENT_ID': JSON.stringify(env.AUTH0_CLIENT_ID),
+        'process.env.AUTH0_DOMAIN': JSON.stringify(env.AUTH0_DOMAIN),
+        'process.env.AUTH0_CLIENT_ID': JSON.stringify(env.AUTH0_CLIENT_ID),
       },
       resolve: {
         alias: {
